8548631bc905864b8b6ced8600ec20cb335ef604
bluetailevents <133782114+bluetailevents@users.noreply.github.com>
2023-12-09 21:57:23 +0000

    Update coordinatesService.js

--- src/features/coordinates/coordinatesService.js
+++ src/features/coordinates/coordinatesService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = `${process.env.REACT_APP_SERVER}/api/geojson/`;
+const API_URL = `${process.env.REACT_APP_HOST}/api/geojson/`;
 
 // Get coordinates
 const getCoordinates = async (token) => {
