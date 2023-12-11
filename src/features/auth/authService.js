6bea5725fcdd140dd9b1b80609c63d973eb73c07
Compphsy <gavinblue30@gmail.com>
2023-12-11 17:44:47 +0000

    working login

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,43 +1,39 @@
-import axios from 'axios'
+import axios from 'axios';
 
-// Replace '/api/users/' with the SERVER environment variable
+// Use the correct environment variable name
 const API_URL = `${process.env.REACT_APP_SERVER}/api/users/`;
-
-
-
-
 
 // Register user
 const register = async (userData) => {
-  const response = await axios.post(API_URL, userData)
+  const response = await axios.post(API_URL, userData);
 
   if (response.data) {
-    localStorage.setItem('user', JSON.stringify(response.data))
+    localStorage.setItem('user', JSON.stringify(response.data));
   }
 
-  return response.data
-}
+  return response.data;
+};
 
 // Login user
 const login = async (userData) => {
-  const response = await axios.post(API_URL + 'login', userData)
+  const response = await axios.post(API_URL + 'login', userData);
 
   if (response.data) {
-    localStorage.setItem('user', JSON.stringify(response.data))
+    localStorage.setItem('user', JSON.stringify(response.data));
   }
 
-  return response.data
-}
+  return response.data;
+};
 
 // Logout user
 const logout = () => {
-  localStorage.removeItem('user')
-}
+  localStorage.removeItem('user');
+};
 
 const authService = {
   register,
   logout,
   login,
-}
+};
 
-export default authService
+export default authService;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+window.addEventListener('DOMContentLoaded', (event) => {
+  const rootElement = document.getElementById('root');
+  if (rootElement) {
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(
+      <React.StrictMode>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </React.StrictMode>
+    );
+  }
+});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
